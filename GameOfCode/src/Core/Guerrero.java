62bdc9a8615c1bd9ad0e7801abffa736176e556f
rogermane <roger.maee@gracia.lasalle.cat>
2026-01-30 13:19:01 +0100

    Entidad Guerrero

---
+++ GameOfCode/src/Core/Guerrero.java
@@ -0,0 +1,14 @@
+package Core;
+
+public class Guerrero extends Entidad{
+
+    private int resistencia;
+
+    public Guerrero() {
+        this.name = "Test";
+        this.nivel = 1;
+        this.puntosVida = 4;
+        this.resistencia = 5;
+    }
+
+}
