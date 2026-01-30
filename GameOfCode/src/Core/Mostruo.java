69d64454e970d9c3cc60309c3bd35d26b0d867ca
rogermane <roger.maee@gracia.lasalle.cat>
2026-01-30 13:23:11 +0100

    Entidad Monstruo

---
+++ GameOfCode/src/Core/Mostruo.java
@@ -0,0 +1,14 @@
+package Core;
+
+public class Mostruo extends Entidad{
+
+    private Boolean salvage;
+
+    public Mostruo() {
+        this.name = "Test";
+        this.nivel = 1;
+        this.puntosVida = 4;
+        this.salvage = false;
+    }
+
+}
