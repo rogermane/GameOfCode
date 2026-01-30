840398959b82f375a4d34a2aa0b248ed886980e3
rogermane <roger.maee@gracia.lasalle.cat>
2026-01-30 13:20:25 +0100

    Entidad Mago

---
+++ GameOfCode/src/Core/Mago.java
@@ -0,0 +1,14 @@
+package Core;
+
+public class Mago extends Entidad{
+
+    private int mana;
+
+    public Mago() {
+        this.name = "Test";
+        this.nivel = 1;
+        this.puntosVida = 4;
+        this.mana = 100;
+    }
+
+}
